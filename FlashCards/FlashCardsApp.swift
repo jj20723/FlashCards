474f70326892aa647a26e620925640f362f95ccd
Joe <joework1@verizon.net>
2024-05-01 09:25:57 -0400

    Initial Commit
    Initial creation of project before any changes were made to the project

---
+++ FlashCards/FlashCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardsApp.swift
+//  FlashCards
+//
+//  Created by Joe Sokolosky on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
